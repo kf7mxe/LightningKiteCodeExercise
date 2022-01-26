d0c56f34345590fb87358ca8217e43f74aecc748
Trax Bagley <kf7mxe@gmail.com>
2022-01-26 09:39:43 -0700

    Added modifiers to change how the cards and the layout looked

--- app/src/main/java/com/kf7mxe/lightningkitecodeexercise/MainActivity.kt
+++ app/src/main/java/com/kf7mxe/lightningkitecodeexercise/MainActivity.kt
@@ -3,19 +3,25 @@ package com.kf7mxe.lightningkitecodeexercise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import com.google.gson.GsonBuilder
@@ -34,15 +40,19 @@ class MainActivity : ComponentActivity() {
         viewModel.fetch("https://jsonplaceholder.typicode.com/posts")
         setContent {
             LightningKiteCodeExerciseTheme {
-                Text(text = "Posts!")
+                
+                Column( verticalArrangement = Arrangement.SpaceAround) {
+                    Text(text = "Posts!",fontSize = 30.sp, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
 
-                var posts = viewModel.posts.value
-                val list by viewModel.posts.observeAsState(initial = emptyList())
-                LazyColumn(modifier = Modifier){
-                    items(list.toList()){ post ->
-                        PostCard(post = post)
+                    val list by viewModel.posts.observeAsState(initial = emptyList())
+                    LazyColumn(modifier = Modifier){
+                        items(list.toList()){ post ->
+                            PostCard(post = post)
+                        }
                     }
                 }
+                
+
 
             }
         }
--- app/src/main/java/com/kf7mxe/lightningkitecodeexercise/components/PostCard.kt
+++ app/src/main/java/com/kf7mxe/lightningkitecodeexercise/components/PostCard.kt
@@ -1,19 +1,26 @@
 package com.kf7mxe.lightningkitecodeexercise.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kf7mxe.lightningkitecodeexercise.models.Post
 
 @Composable
 fun PostCard (post: Post){
-    Card(shape = MaterialTheme.shapes.large) {
+    Card(shape = MaterialTheme.shapes.medium, modifier = Modifier.fillMaxSize()
+        .padding(5.dp),elevation = 2.dp)
+    {
         Column() {
-            Text(text = post.title)
-            Text(text = post.body)
+            Text(text = post.title, fontWeight = FontWeight.Bold, modifier = Modifier.padding(5.dp))
+            Text(text = post.body,modifier = Modifier.padding(5.dp))
         }
     }
 }
