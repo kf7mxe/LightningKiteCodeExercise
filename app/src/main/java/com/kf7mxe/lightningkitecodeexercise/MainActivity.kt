9b87220034445fbdc1f1327881cf357b4284ec40
Trax Bagley <kf7mxe@gmail.com>
2022-01-25 20:04:46 -0700

    Made Fetch function to get the json data

--- app/src/main/java/com/kf7mxe/lightningkitecodeexercise/MainActivity.kt
+++ app/src/main/java/com/kf7mxe/lightningkitecodeexercise/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.gson.GsonBuilder
 import com.kf7mxe.lightningkitecodeexercise.ui.theme.LightningKiteCodeExerciseTheme
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fetch("https://jsonplaceholder.typicode.com/posts")
         setContent {
             LightningKiteCodeExerciseTheme {
                 // A surface container using the 'background' color from the theme
@@ -37,4 +41,19 @@ fun DefaultPreview() {
     LightningKiteCodeExerciseTheme {
         Greeting("Android")
     }
+}
+
+fun fetch(url:String){
+    val request = Request.Builder().url(url).build()
+    val client = OkHttpClient()
+    client.newCall(request).enqueue(object: Callback {
+        override fun onResponse(call: Call, response: Response) {
+            val body = response?.body?.string()
+            println(body)
+        }
+
+        override fun onFailure(call: Call, e: IOException) {
+            TODO("Not yet implemented")
+        }
+    })
 }
